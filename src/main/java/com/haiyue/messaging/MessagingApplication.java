25de25e5b7b6a805f9a1857eac3fd90aea928349
Haiyue Tian <er.haiyue.tian@gmail.com>
2023-03-11 23:02:14 -0500

    implement feature register user

--- src/main/java/com/haiyue/messaging/MessagingApplication.java
+++ src/main/java/com/haiyue/messaging/MessagingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MessagingApplication {
 
+	// implement register user
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingApplication.class, args);
 	}
